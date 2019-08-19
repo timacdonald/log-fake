657987e9ad0d2d41a9c5af7a4ac02d5360dfde39
romkatsu <romka_wm@mail.ru>
2019-08-19 10:41:39 +0300

    channel method return implements psr/LoggerInterface (#6)
    * add abstract logger and implements channel logger for psr
    * add test check for instance channel logger

--- src/ChannelFake.php
+++ src/ChannelFake.php
@@ -2,19 +2,38 @@
 
 namespace TiMacDonald\Log;
 
-class ChannelFake
+use Psr\Log\LoggerInterface;
+
+class ChannelFake extends FakeLogger implements LoggerInterface
 {
+    /**
+     * @var $log LogFake
+     */
     protected $log;
 
+    /**
+     * @var $name string
+     */
     protected $name;
 
+    /**
+     * ChannelFake constructor.
+     *
+     * @param $log
+     * @param $name
+     */
     public function __construct($log, $name)
     {
         $this->log = $log;
-
         $this->name = $name;
     }
 
+    /**
+     * @param $method
+     * @param $arguments
+     *
+     * @return mixed
+     */
     public function __call($method, $arguments)
     {
         $this->log->setCurrentChannel($this->name);
@@ -24,5 +43,23 @@ class ChannelFake
         $this->log->setCurrentChannel(null);
 
         return $result;
+    }
+
+    /**
+     * Log a message to the logs.
+     *
+     * @param string $level
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public function log($level, $message, array $context = [])
+    {
+        $this->log->setCurrentChannel($this->name);
+
+        $this->log->log($level, $message, $context);
+
+        $this->log->setCurrentChannel(null);
     }
 }
---
+++ src/FakeLogger.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace TiMacDonald\Log;
+
+abstract class FakeLogger
+{
+    /**
+     * Log an emergency message to the logs.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public function emergency($message, array $context = [])
+    {
+        $this->log(__FUNCTION__, $message, $context);
+    }
+
+    /**
+     * Log an alert message to the logs.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public function alert($message, array $context = [])
+    {
+        $this->log(__FUNCTION__, $message, $context);
+    }
+
+    /**
+     * Log a critical message to the logs.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public function critical($message, array $context = [])
+    {
+        $this->log(__FUNCTION__, $message, $context);
+    }
+
+    /**
+     * Log an error message to the logs.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public function error($message, array $context = [])
+    {
+        $this->log(__FUNCTION__, $message, $context);
+    }
+
+    /**
+     * Log a warning message to the logs.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public function warning($message, array $context = [])
+    {
+        $this->log(__FUNCTION__, $message, $context);
+    }
+
+    /**
+     * Log a notice to the logs.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public function notice($message, array $context = [])
+    {
+        $this->log(__FUNCTION__, $message, $context);
+    }
+
+    /**
+     * Log an informational message to the logs.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public function info($message, array $context = [])
+    {
+        $this->log(__FUNCTION__, $message, $context);
+    }
+
+    /**
+     * Log a debug message to the logs.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public function debug($message, array $context = [])
+    {
+        $this->log(__FUNCTION__, $message, $context);
+    }
+
+    /**
+     * @param       $level
+     * @param       $message
+     * @param array $context
+     *
+     * @return mixed
+     */
+    abstract public function log($level, $message, array $context = []);
+
+}
